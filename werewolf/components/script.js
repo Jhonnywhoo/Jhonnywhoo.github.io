905761d2da226c509dbdfe3afbb4683294fb5c65
Jhonny Brito <jhonnywobrito@gmail.com>
2025-01-02 23:52:30 -0300

    Adicionada lógica de troca de jogadores

--- werewolf/components/script.js
+++ werewolf/components/script.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const tabelaCorpo = document.querySelector('table tbody');
     const page = document.body.dataset.page;
     const papeisLobisomens = ['lobo solitário', 'filhote de lobisomem', 'lobo alfa'];
+    let posicaoAtual = parseInt(localStorage.getItem('posicaoAtual')) || 0;
+
     const carregarJogadores = () => {
         const jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
-        tabelaCorpo.innerHTML = ''; // Limpa a tabela antes de popular novamente
+        tabelaCorpo.innerHTML = '';
     
         jogadores.forEach((jogador, index) => {
             const linha = document.createElement('tr');
@@ -61,6 +63,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    const imagemPorPapel = {
+        'aldeão': 'img/aldeao.png',
+        'vidente': 'img/vidente.png',
+        'médico': 'img/medico.png',
+        'caçador': 'img/cacador.png',
+        'bruxa': 'img/bruxa.png',
+        'aprendiz de vidente': 'img/aprendiz-de-vidente.png',
+        'pacifista': 'img/pacifista.png',
+        'sacerdote': 'img/sacerdote.png',
+        'prefeito': 'img/prefeito.png',
+        'guarda-costas': 'img/guarda-costas.png',
+        'detetive': 'img/detetive.png',
+        'portador do amuleto': 'img/portador-d0-amuleto.png',
+        'vidente de aura': 'img/vidente-de-aura.png',
+        'príncipe bonitão': 'img/principe-bonitao.png',
+        'maçom': 'img/macom.png',
+        'menininha': 'img/menininha.png',
+        'cientista maluco': 'img/cientista-maluco.png',
+        'caçador de cabeças': 'img/cacador-de-cabecas.png',
+        'humano leproso': 'img/humano-leproso.png',
+        'valentão': 'img/valentao.png',
+        'menino travesso': 'img/menino-travesso.png',
+        'prostituta': 'img/prostituta.png',
+        'vovó zangada': 'img/vovo-zangada.png',
+        'bêbado': 'img/bebado.png',
+        'idiota': 'img/idiota.png',
+        'pistoleiro': 'img/pistoleiro.png',
+        'humano amaldiçoado': 'img/humano-amaldiçoado.png',
+        'feiticeira': 'img/feiticeira.png',
+        'assassino em série': 'img/assassino-em-serie.png',
+        'inquisidor': 'img/inquisidor.png',
+        'sósia': 'img/sosia.png',
+        'líder de seita': 'img/lider-de-seita.png',
+        'cupido': 'img/cupido.png',
+        'depressivo': 'img/depressivo.png',
+        'necromante': 'img/necromante.png',
+        'piromaníaco': 'img/piromaniaco.png',
+        'presidente': 'img/presidente.png',
+        'lobisomem': 'img/lobisomem.png',
+        'lobo alfa': 'img/lobo-alfa.png',
+        'lobo solitário': 'img/lobo-solitario.png',
+        'filhote de lobisomem': 'img/filhote-lobisomem.png'
+    };
+    
     const chancesPapeisConfig = {
         'aldeão': 10,
         'vidente': 2,
@@ -151,7 +197,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //--------------------------------------
 
+if (page === 'index'){
+    localStorage.removeItem('contadorMestre');
+}
+
+    //--------------------------------------
+
+if (page === 'papeis'){
+    localStorage.removeItem('contadorMestre');
+}
+
+    //--------------------------------------
+
 if (page === 'cadastro') {
+    localStorage.removeItem('contadorMestre');
+
     document.getElementById('salvar-sessao-ut').addEventListener('click', (event) => {
         const jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
         if (jogadores.length < 2) {
@@ -188,7 +248,50 @@ if (page === 'cadastro') {
     //--------------------------------------
 
 if (page === 'mestre') {
-    
+    let contador = parseInt(localStorage.getItem("contadorMestre")) || 0;
+
+    contador += 1;
+    localStorage.setItem("contadorMestre", contador);
+
+    if (contador >= 2) {
+        exibirMensagemReinicio();
+    }
+
+    function exibirMensagemReinicio() {
+        const mensagem = document.createElement("div");
+        mensagem.style.position = "fixed";
+        mensagem.style.top = "0";
+        mensagem.style.left = "0";
+        mensagem.style.width = "100%";
+        mensagem.style.height = "100%";
+        mensagem.style.backgroundColor = "#000";
+        mensagem.style.color = "#fff";
+        mensagem.style.display = "flex";
+        mensagem.style.flexDirection = "column";
+        mensagem.style.justifyContent = "center";
+        mensagem.style.alignItems = "center";
+        mensagem.style.fontFamily = "'Righteous', sans-serif";
+        mensagem.style.fontSize = "2rem";
+        mensagem.style.textAlign = "center";
+        mensagem.style.zIndex = "1000";
+
+        mensagem.textContent = "Todo o progresso foi perdido, inicie o jogo novamente.";
+
+        const botaoVoltar = document.createElement("button");
+        botaoVoltar.textContent = "Voltar";
+
+        botaoVoltar.addEventListener("click", () => {
+            window.location.href = "index.html";
+
+            localStorage.removeItem("contadorMestre");
+        });
+
+        mensagem.appendChild(botaoVoltar);
+
+        document.body.appendChild(mensagem);
+    }
+
+    localStorage.removeItem('posicaoAtual');
     const carregarJogadores = () => JSON.parse(localStorage.getItem('jogadores')) || [];
     const carregarPapeis = () => JSON.parse(localStorage.getItem('papeisSessao')) || [];
 
@@ -323,4 +426,66 @@ if (page === 'mestre') {
     });
 }
 
+    //--------------------------------------
+
+if (page === 'mediador') {
+        const modal = document.getElementById('modal');
+        const continuarBtnModal = document.getElementById('continuar-btn');  // Botão no modal
+        const conteudoPrincipal = document.getElementById('pagina-mediador');  // Conteúdo principal
+        const nomeJogador = document.getElementById('nome-jogador');
+        const papelJogador = document.getElementById('papel-jogador');
+        const imagemJogador = document.getElementById('imagem-jogador');  // Elemento para a imagem do jogador
+        const jogadores = JSON.parse(localStorage.getItem('jogadores')) || [];
+        let posicaoAtual = parseInt(localStorage.getItem('posicaoAtual')) || 0;
+        const resultadoSorteio = JSON.parse(localStorage.getItem('resultadoSorteio')) || [];
+    
+        // Função para exibir o jogador atual no modal
+        const exibirJogadorModal = () => {
+            const jogadorAtual = jogadores[posicaoAtual] || 'Nenhum jogador encontrado.';
+            document.querySelector('.nome-pessoa .comando').textContent = `Passe o aparelho para: ${jogadorAtual}`;
+        };
+    
+        // Função para atualizar o nome, papel e imagem do jogador no conteúdo principal
+        const atualizarJogadorInfo = () => {
+            const jogadorAtual = jogadores[posicaoAtual];
+            const papelAtual = resultadoSorteio.find(jogador => jogador.jogador === jogadorAtual)?.papel || 'Sem papel definido';
+            
+            nomeJogador.textContent = `Jogador: ${jogadorAtual}`;
+            papelJogador.textContent = `Papel: ${papelAtual}`;
+            
+            // Atualiza a imagem de acordo com o papel do jogador
+            setTimeout(() => {
+                const imagemPapel = imagemPorPapel[papelAtual.toLowerCase()] || 'img/default.png';
+                imagemJogador.src = imagemPapel;  // Troca a imagem do jogador
+            }, 500);  // Atraso de 1 segundo para a troca da imagem
+        };
+    
+        // Atualiza o jogador no início do modal
+        exibirJogadorModal();
+        
+    
+        // Função para passar para o próximo jogador
+        const proximoJogador = () => {
+            posicaoAtual = (posicaoAtual + 1) % jogadores.length;
+            localStorage.setItem('posicaoAtual', posicaoAtual);
+            exibirJogadorModal(); // Atualiza o jogador no modal
+        };
+    
+        // Botão "Continuar" no modal
+        continuarBtnModal.addEventListener('click', () => {
+            
+            modal.style.display = 'none'; // Oculta o modal
+    
+            // Atualiza as informações do jogador após o modal (atrasa para garantir que o modal feche primeiro)
+            setTimeout(() => {
+                atualizarJogadorInfo();  // Atualiza a informação do jogador no conteúdo principal
+            }, 500);  // Atraso de 500ms
+        });
+
+        proximoJogador();
+    }
+    
+
+
 });
+
